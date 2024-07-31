7f6aa6fd50a9fab7fa590783d3d80c16153e33b8
szymek111 <szympek1515@gmail.com>
2024-08-01 01:37:12 +0200

    added rocket analysys

--- project_sql/5_rocket_analysis.sql
+++ project_sql/5_rocket_analysis.sql
@@ -5,3 +5,19 @@ Shows the number of missions conducted using each rocket.
 Most frequently used rocket
 Identifies which rocket was used the most frequently.
 */
+
+
+SELECT 
+    DISTINCT rocket,
+    COUNT(rocket) AS rocket_used
+FROM space_mission
+GROUP BY rocket
+ORDER BY rocket_used DESC;
+
+SELECT 
+    DISTINCT rocket,
+    COUNT(rocket) AS rocket_used
+FROM space_mission
+GROUP BY rocket
+ORDER BY rocket_used DESC
+LIMIT 10;
