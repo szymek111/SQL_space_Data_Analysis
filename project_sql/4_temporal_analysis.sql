9cfada89d8f3b23ca507679a43904fdb734a4de8
szymek111 <szympek1515@gmail.com>
2024-08-01 01:34:38 +0200

    added temporal analysys

--- project_sql/4_temporal_analysis.sql
+++ project_sql/4_temporal_analysis.sql
@@ -3,3 +3,19 @@ Number of space missions in each month Displays the number of missions conducted
 
 Most active month in terms of number of missions Determines which month was the most active in terms of the number of space missions.
 */
+
+
+SELECT 
+    month_value,
+    COUNT(month_value) AS month_occurence
+FROM(SELECT EXTRACT(MONTH FROM date) AS month_value FROM space_mission) AS month_table
+GROUP BY month_value
+ORDER BY month_value;
+
+SELECT 
+    month_value,
+    COUNT(month_value) AS month_occurence
+FROM(SELECT EXTRACT(MONTH FROM date) AS month_value FROM space_mission) AS month_table
+GROUP BY month_value
+ORDER BY month_occurence DESC
+LIMIT 1;
